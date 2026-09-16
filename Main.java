9e7dc14b2dc897616c9440b12a3a8fc932362f25
novaleswar <vh15203@velhightech.com>
2026-09-16 21:06:32 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter total stock level: ");
+        int totalStock = sc.nextInt();
+
+        System.out.print("Enter number of warehouses: ");
+        int warehouses = sc.nextInt();
+
+        try {
+            int averageStock = totalStock / warehouses;
+            System.out.println("Average stock level: " + averageStock);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Number of warehouses cannot be zero.");
+            System.out.println("Average stock level cannot be calculated.");
+        }
+
+        sc.close();
+    }
+}
